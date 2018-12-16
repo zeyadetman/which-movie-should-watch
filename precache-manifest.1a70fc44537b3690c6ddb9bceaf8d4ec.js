e4d9e40e0e1a9084ea2470f746fdd430a85c5709
zeyadetman <zeyadetman@yahoo.com>
2018-12-16 06:02:50 +0200

    Updates

---
+++ precache-manifest.1a70fc44537b3690c6ddb9bceaf8d4ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60fde80390eba78d8b13",
+    "url": "/which-movie-should-watch/static/js/runtime~main.60fde803.js"
+  },
+  {
+    "revision": "6f40c32c9b0d22592a88",
+    "url": "/which-movie-should-watch/static/js/main.6f40c32c.chunk.js"
+  },
+  {
+    "revision": "babcc456e4933f1bc544",
+    "url": "/which-movie-should-watch/static/js/1.babcc456.chunk.js"
+  },
+  {
+    "revision": "6f40c32c9b0d22592a88",
+    "url": "/which-movie-should-watch/static/css/main.4e21e873.chunk.css"
+  },
+  {
+    "revision": "babcc456e4933f1bc544",
+    "url": "/which-movie-should-watch/static/css/1.d02e5d8e.chunk.css"
+  },
+  {
+    "revision": "72ba5a227eb0b7cceaa12eedb707ba76",
+    "url": "/which-movie-should-watch/index.html"
+  }
+];
